27a404d59a479f9e3f7c26a781259e63ad6ad304
ali <ali.ergul@pusula.net.tr>
2025-05-26 15:25:46 +0300

    prisma db, create note action for test, some configurations

---
+++ app/actions.ts
@@ -0,0 +1,30 @@
+"use server";
+
+import { PrismaClient } from "@prisma/client";
+import { revalidatePath } from "next/cache";
+
+const prisma = new PrismaClient();
+
+export async function createNote(formData: FormData) {
+  const title = formData.get("title") as string;
+  const content = formData.get("content") as string;
+
+  if (!title) {
+    return { message: "Başlık boş olamaz!", status: "error" };
+  }
+
+  try {
+    await prisma.note.create({
+      data: {
+        title,
+        content,
+      },
+    });
+    // Next.js'in önbelleğini yenile
+    revalidatePath("/"); // Ana sayfayı veya notların listelendiği sayfayı yenile
+    return { message: "Not başarıyla oluşturuldu.", status: "success" };
+  } catch (error) {
+    console.error("Not oluşturma hatası:", error);
+    return { message: "Not oluşturulurken bir hata oluştu.", status: "error" };
+  }
+}
